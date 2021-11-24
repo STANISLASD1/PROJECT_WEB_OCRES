d747066c1b16f6db184aa64eb8b14e4adf9defbf
STANISLASD1 <74603003+STANISLASD1@users.noreply.github.com>
2021-11-24 10:21:06 +0100

    add: Correction erreurs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Home from './pages/Home';
+//import logo from './logo.svg';
+//import './App.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound"; 
+import About from "./pages/About"; 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    
+ <BrowserRouter>
+<Routes>
+   <Route path ="/" exact  component ={Home}/>
+   <Route path ="/a-propos" exact component ={About} />
+   <Route component ={NotFound}/>
+</Routes>
+ </BrowserRouter>
+
   );
 }
 
 export default App;
+
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"; 
+
 
 const Navigation = () => {
     return (
         <div className="navigation">
-            <NavLink exact to="/">
+            <NavLink exact to="/" activeClassName ="nav">
                 Accueil
             </NavLink>
+            <NavLink exact to="/a-propos" activeClassName ="nav"> 
+                A propos 
+            </NavLink>
+        
         </div>
     );
 };
