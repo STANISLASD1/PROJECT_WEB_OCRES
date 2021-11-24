4f4d22f292627437e34056f3c2caf039fb88881d
STANISLASD1 <74603003+STANISLASD1@users.noreply.github.com>
2021-11-24 10:21:58 +0100

    add: NotFound.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>
+                À propros 
+            </h1>
+            <br>
+            Lorem 
+            </br>
+            <br>
+            Lorem
+            </br>
+        </div>
+    );
+};
+
+export default About;
