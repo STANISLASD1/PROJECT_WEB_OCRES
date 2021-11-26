23ee8d83acfeece7f5aefc65fec9ebe24c437e2e
STANISLASD1 <74603003+STANISLASD1@users.noreply.github.com>
2021-11-26 15:10:00 +0100

    add: Footer, footerstyles

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,13 @@
 //import { useState, useEffect } from 'react';
 import * as React from 'react';
-
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "../styles/FooterStyles";
 import CardContent from '@mui/material/CardContent';
 //import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
@@ -14,7 +21,64 @@ export default function Footer() {
         <div>
             <Card>
                 <CardContent>
-                    bla bla bla ta vie
+                <Box>
+      <h1 style={{ color: "green", 
+                   textAlign: "center", 
+                   marginTop: "-50px" }}>
+        Information sur notre Site
+      </h1>
+      <Container>
+        <Row>
+           <Column>
+            <Heading>Times To Ski </Heading>
+            <FooterLink href="#">Condition/Politique</FooterLink>
+            <FooterLink href="#">France</FooterLink>
+            <FooterLink href="#">Tignes</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Contactez-nous</Heading>
+            <FooterLink href="#">TimeToSki@</FooterLink>
+
+          </Column>
+          <Column>
+            <Heading>Inscrpition Newletter</Heading>
+            <FooterLink href="#">TimeToSki@</FooterLink>
+          </Column>
+         
+          <Column>
+            <Heading>Réseaux sociaux</Heading>
+            <FooterLink href="#">
+              <i className="fab fa-facebook-f">
+                <span style={{ marginLeft: "10px" }}>
+                  Facebook
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-instagram">
+                <span style={{ marginLeft: "10px" }}>
+                  Instagram
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-twitter">
+                <span style={{ marginLeft: "10px" }}>
+                  Twitter
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-youtube">
+                <span style={{ marginLeft: "10px" }}>
+                  Youtube
+                </span>
+              </i>
+            </FooterLink>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
                 </CardContent>
             </Card>
 
