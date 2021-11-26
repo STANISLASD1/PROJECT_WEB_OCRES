b6bacad657d259be1513ee747b40903e460d4e6a
STANISLASD1 <74603003+STANISLASD1@users.noreply.github.com>
2021-11-26 12:22:15 +0100

    add: Main and footer in home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
 
-
+import Main from '../components/Main';
 
 const Home = () => {
     return(
         <div className="home">
             <Navigation />
+          
             <h1>cette page sera dédiée a notre dashboard </h1>
             <p>Nous y retrouverons toutes les API</p>
+            <Main></Main>
+            <Footer></Footer>
         </div>
   
 
