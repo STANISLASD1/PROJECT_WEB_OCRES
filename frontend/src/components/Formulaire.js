ba38306ea1af4857c5e808284e26b0d2d42d1dc0
Fouillard <pierre-erwan.fouillard@edu.ece.fr>
2021-12-06 17:36:05 +0100

    modify: formulaire

--- frontend/src/components/Formulaire.js
+++ frontend/src/components/Formulaire.js
@@ -3,10 +3,16 @@ import * as React from 'react';
 export default function Formulaire() {
 
     return (
-        <div className="container">
-            <h2>Selectionner la tache à effectuer :</h2>
-            <button>Ajouter</button>
-            <button>Supprimer</button>
+        <div className="formulaire container">
+            <h2>Selectionner la tâche à effectuer :</h2>
+            <div className="button2 justify-content-center">
+                <div>
+                    <button>Ajouter</button>
+                </div>
+                <div>
+                    <button>Supprimer</button>
+                </div>
+            </div>
         </div>
     );
 }
