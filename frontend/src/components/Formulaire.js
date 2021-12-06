b41795b1ed15a806fb01cd204df31f0764891658
Fouillard <pierre-erwan.fouillard@edu.ece.fr>
2021-12-06 22:09:57 +0100

    modify: formulaire

--- frontend/src/components/Formulaire.js
+++ frontend/src/components/Formulaire.js
@@ -13,6 +13,29 @@ export default function Formulaire() {
                     <button>Supprimer</button>
                 </div>
             </div>
+            <br/>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 carte">
+                        <h3>Ajouter des photos</h3>
+                        <br/>
+                        <form>
+                            <input type="file" />
+                            <input type="submit" value="Enregistrer" />
+                        </form>
+                    </div>
+                    <div className="col-md-6 carte">
+                        <h3>Supprimer des photos</h3>
+                        <br/>
+                        <form>
+                            <p>[Tableau photos]</p>
+                            <label>selectionner un fichier</label>
+                            <input type="submit" value="Supprimer" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <br/>
         </div>
     );
 }
