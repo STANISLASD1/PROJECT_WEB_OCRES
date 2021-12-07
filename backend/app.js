dca2e43a8796b9854a7d1961fe244f41689a2424
Fouillard <pierre-erwan.fouillard@edu.ece.fr>
2021-12-07 11:43:17 +0100

    test aide arthur

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,13 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-
 var app = express();
+
+const mongoose = require('mongoose');
+
+const CONNECTION_URL = 'mongodb+srv://stanislas:Stanislas@cluster0.o9eno.mongodb.net/Project1?retryWrites=true&w=majority';
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true}, () => console.log('Database connected'));
 
 app.use(logger("dev"));
 app.use(express.json());
---
+++ backend/routes/users.js
@@ -0,0 +1,59 @@
+const { json } = require('express');
+var express = require('express');
+var router = express.Router();
+
+const Post = require('../models/user.model');
+
+
+/* GET photos listing. */
+const getUsers = async (req, res) => {
+  const list = await Post.find();
+  res.json(list);
+}
+router.get('/', getUsers);
+
+
+/* POST a new photos */
+const createUser = async (req, res) => {
+  const post = new Post({
+    name: req.body.name,
+    picture: req.body.picture
+  });
+
+  const create = await post.save();
+  res.json(create);
+}
+router.post('/', createUser);
+
+
+/* RESEARCH a photo by id */
+const researchUser = async (req, res) => {
+  const research = await Post.findById(req.params.id);
+  res.json(research);
+}
+router.get('/:id', researchUser);
+
+
+/* DELETE a photos by id */
+const deleteUser = async (req, res) => {
+  const suppr = await Post.remove({_id: req.params.id});
+  res.json(suppr);
+}
+router.delete('/:id', deleteUser);
+
+
+/* UPDATE a photos by id */
+const updateUser = async (req, res) => {  
+  const update = await Post.updateOne(
+    { _id: req.params.id },
+    { $set: { 
+        name: req.body.name
+      } 
+    }
+  );
+  res.json(update);
+}
+router.patch('/:id', updateUser);
+
+
+module.exports = router;
